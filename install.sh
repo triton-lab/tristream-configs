e56ec782eb59ca9acc93c21714eb53274bdee371
Yamato Matsuoka <yamaton@gmail.com>
2023-11-06 15:52:06 -0500

    Fix rsync and relevant tools

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ sudo yum upgrade -y
 
 
 # Install essentials
-sudo yum install -y git zsh fish wget curl htop vim emacs jq source-highlight ShellCheck cmake rclone ImageMagick
+sudo yum install -y git zsh fish wget curl htop vim emacs jq source-highlight ShellCheck cmake rclone ImageMagick gnome-tweaks-tool
 
 
 # Install fonts
