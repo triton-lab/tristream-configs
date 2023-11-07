17a13bcbafc566e6411d23e4d20585d2b0f96932
Yamato Matsuoka <yamaton@gmail.com>
2023-11-07 15:32:59 -0500

    Cleanup

--- install.sh
+++ install.sh
@@ -3,54 +3,47 @@
 set -o errexit
 set -o pipefail
 
-
 BINDIR=/opt/bin
 
-BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 readonly BASEDIR
 
 # Update
 sudo yum update -y
 sudo yum upgrade -y
 
-
 # Install essentials
 sudo yum install -y wget curl vim emacs source-highlight gnome-tweaks-tool
 
-
 # Install fonts
 sudo yum install -y \
-  adobe-source-code-pro-fonts \
-  google-noto-cjk-fonts \
-  levien-inconsolata-fonts \
-  roboto-fontface-fonts \
-  open-sans-fonts \
-  mozilla-fira-mono-fonts
-
+    adobe-source-code-pro-fonts \
+    google-noto-cjk-fonts \
+    levien-inconsolata-fonts \
+    roboto-fontface-fonts \
+    open-sans-fonts \
+    mozilla-fira-mono-fonts
 
 # Cascadia Code fonts
 FONT_PATH="/usr/share/fonts/CascadiaCode"
 if [ ! -d "$FONT_PATH" ]; then
-  wget -N https://github.com/microsoft/cascadia-code/releases/download/v2111.01/CascadiaCode-2111.01.zip
-  unzip -d CascadiaCode -o CascadiaCode-2111.01.zip
-  sudo mkdir -p "$FONT_PATH"
-  sudo cp -f CascadiaCode/ttf/static/CascadiaCode*.ttf "$FONT_PATH"/
-  sudo fc-cache -f -
+    wget -N https://github.com/microsoft/cascadia-code/releases/download/v2111.01/CascadiaCode-2111.01.zip
+    unzip -d CascadiaCode -o CascadiaCode-2111.01.zip
+    sudo mkdir -p "$FONT_PATH"
+    sudo cp -f CascadiaCode/ttf/static/CascadiaCode*.ttf "$FONT_PATH"/
+    sudo fc-cache -f -
 fi
-
 
 # Firefox (Checks if Firefox is installed before installation.)
-if ! command -v firefox &> /dev/null; then
-  sudo amazon-linux-extras install firefox -y
+if ! command -v firefox &>/dev/null; then
+    sudo amazon-linux-extras install firefox -y
 fi
-
 
 # vscode (Checks if Visual Studio Code is installed before installation.)
-if ! command -v code &> /dev/null; then
-  wget -N https://update.code.visualstudio.com/1.84.0/linux-rpm-x64/stable -O vscode.rpm
-  sudo yum localinstall vscode.rpm -y
+if ! command -v code &>/dev/null; then
+    wget -N https://update.code.visualstudio.com/1.84.0/linux-rpm-x64/stable -O vscode.rpm
+    sudo yum localinstall vscode.rpm -y
 fi
-
 
 # miniforge
 wget -N https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
@@ -59,7 +52,6 @@ CONDA_PREFIX=/opt/miniforge3
 sudo rm -rf "$CONDA_PREFIX"
 sudo bash Miniforge3-Linux-x86_64.sh -b -p "$CONDA_PREFIX"
 
-
 # Make pip available via conda
 sudo cp -f "$BASEDIR/conda.sh" /etc/profile.d/
 source /etc/profile
@@ -67,10 +59,8 @@ source /etc/profile
 # Set the environment variables
 sudo cp -f "$BASEDIR/myenvvars.sh" /etc/profile.d/
 
-
 # Basic python packages via mamba
 sudo "$CONDA_PREFIX"/bin/mamba install -y -p "$CONDA_PREFIX" numpy ipython pandas matplotlib seaborn scikit-learn scikit-image scipy jupyterlab
-
 
 # pipx
 export PIPX_HOME=/opt/share/pipx
@@ -79,7 +69,6 @@ sudo mkdir -p "$PIPX_HOME"
 sudo mkdir -p "$PIPX_BIN_DIR"
 sudo "$CONDA_PREFIX"/bin/pip install --prefix "$CONDA_PREFIX" --force-reinstall pipx
 sudo -E "$CONDA_PREFIX"/bin/pipx install --force git+https://github.com/yamaton/condax
-
 
 # condax
 export CONDAX_BIN_DIR="$BINDIR"
@@ -90,7 +79,7 @@ wget -N https://raw.githubusercontent.com/yamaton/test-binder/main/binder/_tools
 
 CONDAX_LOG="condax_install_log.txt"
 rm -f "$CONDAX_LOG"
-TOOLS=( "$(cat _tools_condax.txt)" )
+TOOLS=("$(cat _tools_condax.txt)")
 max_attempts=3
 for _tool in ${TOOLS[*]}; do
     attempt_num=1
@@ -131,14 +120,11 @@ for app in ${CONDAX_EXTRA_APPS[*]}; do
     sudo -E "$BINDIR"/condax install --force "$app"
 done
 
-
-
 # R and RStudio
 sudo amazon-linux-extras install -y R3.4
 sudo yum install -y R
 wget -N https://download1.rstudio.org/electron/centos7/x86_64/rstudio-2023.09.1-494-x86_64.rpm -O rstudio.rpm
 sudo yum localinstall rstudio.rpm -y
-
 
 ## Add application icons
 sudo mkdir -p /opt/share/icons
@@ -161,8 +147,6 @@ sudo cp -f "$BASEDIR/bandage.desktop" /usr/share/applications/
 sudo cp -f "$BASEDIR/igv.desktop" /usr/share/applications/
 sudo update-desktop-database
 
-
-
 ## >>>>------------------------------------------------------------------------
 ## Setup dotfiles in /etc/skel
 
@@ -175,46 +159,41 @@ sudo cp -f "BASEDIR/save-settings.desktop" "$SKEL/Desktop/"
 sudo cp -f "$BASEDIR/load-settings" "$BINDIR"
 sudo cp -f "$BASEDIR/save-settings" "$BINDIR"
 
-
 # zsh
 sudo cp -f "$BASEDIR/zshrc" "$SKEL/.zshrc"
 sudo cp -f "$BASEDIR/zshenv" "$SKEL/.zshenv"
 zshdir=configs/zsh
 mkdir -p "$zshdir"
 pushd "$zshdir" || return
-    git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting
-    git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions
-    git clone --depth 1 https://github.com/zsh-git-prompt/zsh-git-prompt
-    git clone --depth 1 https://github.com/yamaton/zsh-completions-bio
-    git clone --depth 1 https://github.com/yamaton/zsh-completions-extra
+git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting
+git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions
+git clone --depth 1 https://github.com/zsh-git-prompt/zsh-git-prompt
+git clone --depth 1 https://github.com/yamaton/zsh-completions-bio
+git clone --depth 1 https://github.com/yamaton/zsh-completions-extra
 popd || return
 sudo mkdir -p "$SKEL/.config"
 sudo cp -rf configs/* "$SKEL/.config"
-
-
 
 # fish
 sudo mkdir -p "$SKEL/.config/fish"
 fishcompdir=configs/fish/completions
 mkdir -p "$fishcompdir"
 pushd "$fishcompdir" || return
-    git clone --depth 1 https://github.com/yamaton/fish-completions-bio
-    git clone --depth 1 https://github.com/yamaton/fish-completions-extra
+git clone --depth 1 https://github.com/yamaton/fish-completions-bio
+git clone --depth 1 https://github.com/yamaton/fish-completions-extra
 popd || return
 sudo cp -rf "$fishcompdir" "$SKEL/.config/fish"
 sudo cp -f "$BASEDIR/config.fish" "$SKEL/.config/fish/config.fish"
 sudo mkdir -p "$SKEL/.config/fish/functions"
 sudo cp -f "$BASEDIR/fish_prompt.fish" "$SKEL/.config/fish/functions"
 
-
 # bash
 sudo cp -f "$BASEDIR/bashrc" "$SKEL/.bashrc"
-
 
 # .condarc
 #   Enable users to install conda environments
 #   [NOTE] Add after system-wide conda/mamba installation
-cat << 'EOF' | sudo tee "$SKEL/.condarc"
+cat <<'EOF' | sudo tee "$SKEL/.condarc"
 channels:
   - bioconda
   - conda-forge
@@ -228,23 +207,18 @@ pkgs_dirs:
   - /opt/miniforge3/pkgs
 EOF
 
-
 # .mambarc
 # Run this after miniforge installation
 echo "use_lockfiles: false" | sudo tee "$SKEL/.mambarc"
-
 
 # .less_termcap
 wget -N https://raw.githubusercontent.com/yamaton/dotfiles/master/.less_termcap
 sudo cp -f .less_termcap "$SKEL"
 
-
 # Install vscode extensions and copy configs to /etc/skel
 ./vscode-extensions.sh
 
-
 # Add extra CLI tools
-
 
 # via direct installation
 APPS=(tealdeer zoxide croc ffsend navi tre)
@@ -257,7 +231,6 @@ for app in ${APPS[*]}; do
 done
 
 ## <<<<------------------------------------------------------------------------
-
 
 echo "Finished ... for now."
 
